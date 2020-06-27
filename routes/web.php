31fa6b65ece6bf64c76dc5759fc447b33d2bcb36
Annet Torres <annet-gomez7@gmail.com>
2020-06-26 21:12:17 -0500

    container de historial, faltan arreglos

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,7 @@ Route::get('/general-program', "ProgramacionGeneralController@index")->name('pro
 
 
 Route::post('/view', "ViewsController@index")->middleware('session_user');
+
+Route::get('/histo', function () {
+    return view('partials.adm-CN.historial');
+})->name('histo');
