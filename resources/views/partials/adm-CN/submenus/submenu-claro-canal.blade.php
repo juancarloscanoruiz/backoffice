4165c02c47058443712da4eaa40e01048a835718
Annet Torres <annet-gomez7@gmail.com>
2020-08-10 13:03:57 -0500

    redireccionamiento en cancelar, arreglado

--- resources/views/partials/adm-CN/submenus/submenu-claro-canal.blade.php
+++ resources/views/partials/adm-CN/submenus/submenu-claro-canal.blade.php
@@ -371,7 +371,7 @@
                     <div class="text-center  mb-4 d-flex justify-content-center pb-2">
                         <button
                             class="d-flex m-0  mr-3 text-uppercase btn-grilla a-btn-basic-small btn-grilla a-btn-basic-small text-uppercase a-text-MBlack text-plus edit-landing-modal-button" id="edit-logos-button">ACEPTAR</button>
-                            <button href="#delete-info" role="button" class="d-flex m-0 text-none text-uppercase btn-landing a-btn-basic-small text-plus a-text-bold-teal cancel" data-toggle="modal">CANCELAR</button>
+                            <a href="#delete-info" role="button" class="d-flex m-0 text-none text-uppercase btn-landing a-btn-basic-small text-plus a-text-bold-teal cancel" data-toggle="modal">CANCELAR</a>
 
                     </div>
                     </form>
@@ -1096,7 +1096,7 @@
               <div class="text-center mb-3 d-flex justify-content-center">
                   <button
                       class="edit-landing-modal-button d-flex mr-3 text-uppercase  m-0 btn-grilla a-btn-basic-small btn-grilla a-btn-basic-small text-uppercase a-text-MBlack text-plus" id="image-programming-button" landin="canal claro">aceptar</button>
-                      <button href="#delete-info" role="button" class="d-flex m-0 text-none text-uppercase btn-landing a-btn-basic-small text-plus a-text-bold-teal cancel" data-toggle="modal">CANCELAR</button>
+                      <a href="#delete-info" role="button" class="d-flex m-0 text-none text-uppercase btn-landing a-btn-basic-small text-plus a-text-bold-teal cancel" data-toggle="modal">CANCELAR</a>
 
                      <!-- <button
                       class="d-inline-block text-uppercase btn-landing a-btn-basic-small text-plus a-text-bold-teal" data-toggle="modal"   >cancelar</button>-->
