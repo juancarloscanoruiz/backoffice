811df23901a8e05170711b5be8dd73effd6f576d
Annet Torres <annet.gomez7@gmail.com>
2020-08-24 21:33:15 -0500

    modales de carrusel

--- routes/web.php
+++ routes/web.php
@@ -107,7 +107,7 @@ Route::get('/menus', function () {
 });
 
 Route::get('/menurris', function () {
-    return view('partials.adm-CN.modals-concert.encabezado');
+    return view('partials.adm-CN.modals-concert.carrusel2');
 });
 //RUTA PARA REGRESAR A LANDING DE PROGRAMACIÓN
 Route::group(['prefix' => 'lan-claro', "middleware" => "session_user"], function () {
